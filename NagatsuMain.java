a98c9346ba4de9654fa9bf4b0c120cf82dbe1c8f
okaka <aki.na@live.jp>
2022-03-30 14:07:55 +0900

    たからくじ作成

--- NagatsuMain.java
+++ NagatsuMain.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class NagatsuMain{
 	public static void main(String[] args){
 		int[] lotts = new int[101];
-		for(int i=0;i<lotts.length-1;i++){
+		for(int i=0;i<lotts.length;i++){
 			lotts[i]=i;
 		}
-		for(int i=0;i<lotts.length-1;i++){
+		for(int i=0;i<lotts.length;i++){
 			int index = new java.util.Random().nextInt(lotts.length-i)+i;
 			int tmp = lotts[index];
 			lotts[index] = lotts[i];
@@ -14,7 +14,7 @@ public class NagatsuMain{
 		System.out.print("何枚？>>");
 		int n = new Scanner(System.in).nextInt();
 		int[] myLotts = new int[n];
-		for(int i=0;i<myLotts.length-1;i++){
+		for(int i=0;i<myLotts.length;i++){
 			myLotts[i] = lotts[i];
 		}
 		System.out.println(Arrays.toString(myLotts));
@@ -22,7 +22,7 @@ public class NagatsuMain{
 		System.out.print("当選番号：");
 		System.out.println(tousen);
 		boolean isAtari = false;
-		for(int i=0;i<myLotts.length-1;i++){
+		for(int i=0;i<myLotts.length;i++){
 			if(myLotts[i]==tousen){
 				isAtari = true;
 			}
